46271d452a39e04e3e4c2b66622aeae9238a8402
nevalashka <b.rukavishnikov@gmail.com>
2019-02-02 15:16:24 +0300

    Добавил простой массив и двумерный массив с данными из таблицы 2.1

--- index.php
+++ index.php
@@ -53,7 +53,48 @@ $user_name = 'Boris Rukavishnikov'; // укажите здесь ваше имя
                 <h2 class="promo__title">Нужен стафф для катки?</h2>
                 <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
                 <ul class="promo__list">
-                    <!--заполните этот список из массива категорий-->
+                    <? $categories = [
+    «Доски_и_лыжи», «Крепления», «Ботинки»,
+    «Одежда», «Инструменты», «Разное»];?>
+                    <? $lots = [
+    1 => [
+        'title' => '2014 Rossignol District Snowboard',
+        'category' => 'Доски_и_лыжи',
+        'price' => '10999',
+        'url' => 'img/lot-1.jpg'
+    ],
+    2 => [
+        'title' => 'DC Ply Mens 2016/2017 Snowboard',
+        'category' => 'Доски_и_лыжи',
+        'price' => '159999',
+        'url' => 'img/lot-2.jpg'
+    ],
+    3 => [
+        'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'category' => 'Крепления',
+        'price' => '8000',
+        'url' => 'img/lot-3.jpg'
+    ],
+    4 => [
+        'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'category' => 'Ботинки',
+        'price' => '10999',
+        'url' => 'img/lot-4.jpg'
+    ],
+    5 => [
+        'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'category' => 'Одежда',
+        'price' => '7500',
+        'url' => 'img/lot-5.jpg'
+    ],
+    6 => [
+        'title' => 'Маска Oakley Canopy',
+        'category' => 'Разное',
+        'price' => '5400',
+        'url' => 'img/lot-6.jpg'
+    ],
+];
+                    ?>
                     <li class="promo__item promo__item--boards">
                         <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
                     </li>
