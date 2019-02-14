b1322f309834f5e84db418f10a04b80d145632f0
nevalashka <b.rukavishnikov@gmail.com>
2019-02-14 14:54:24 +0300

    исправление ошибок в файле shema.sql 4.1

--- schema.sql
+++ schema.sql
@@ -4,39 +4,41 @@ CREATE DATABASE yeticave
 
 USE yeticave;
 
-CREATE TABLE IF NOT EXISTS `bids` (
-  `id` int(11) NOT NULL,
-  `bid_date` date DEFAULT NULL,
-  `bid_amount` int(11) DEFAULT NULL
-);
-
-
 CREATE TABLE IF NOT EXISTS `categoty` (
   `id` int(11) NOT NULL,
   `category` char(128) DEFAULT NULL
 );
 
-
-CREATE TABLE IF NOT EXISTS `lots` (
-  `id` int(10) NOT NULL,
-  `date_creation` datetime DEFAULT NULL,
-  `name_lot` varchar(30) DEFAULT NULL,
-  `description` text,
-  `url_picture` varchar(100) DEFAULT NULL,
-  `start_price` int(5) DEFAULT NULL,
-  `date_finish` datetime DEFAULT NULL,
-  `bid` int(5) DEFAULT NULL
-);
-
-
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL,
-  `email` varchar(64) NOT NULL,
+  `email` char(64) NOT NULL,
   `password` char(64) NOT NULL,
   `name` char(128) DEFAULT NULL,
   `avatar` char(64) DEFAULT NULL,
   `contact` text,
   `registration_date` date DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `lots` (
+  `id` int(10) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `winner_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `date_creation` datetime DEFAULT NULL,
+  `name_lot` char(30) DEFAULT NULL,
+  `description` text,
+  `url_picture` varchar(100) DEFAULT NULL,
+  `start_price` int(5) DEFAULT NULL,
+  `date_finish` datetime DEFAULT NULL,
+  `bid` int(5) DEFAULT NULL
+)
+
+CREATE TABLE IF NOT EXISTS `bids` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `lot_id` int(11) NOT NULL,
+  `bid_date` date DEFAULT NULL,
+  `bid_amount` int(11) DEFAULT NULL
 );
 
 
