4ff9313df927d9676c8f26258924b9175ed51be5
nevalashka <b.rukavishnikov@gmail.com>
2019-02-18 21:06:30 +0300

    выполнение задания 4.2

--- schema.sql
+++ schema.sql
@@ -5,12 +5,12 @@ CREATE DATABASE yeticave
 USE yeticave;
 
 CREATE TABLE IF NOT EXISTS `category` (
-  `id` int(11) NOT NULL,
+  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `category` char(128) DEFAULT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `email` char(64) NOT NULL,
   `password` char(64) NOT NULL,
   `name` char(128) DEFAULT NULL,
@@ -20,24 +20,23 @@ CREATE TABLE IF NOT EXISTS `users` (
 );
 
 CREATE TABLE IF NOT EXISTS `lots` (
-  `id` int(10) NOT NULL,
+  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `winner_id` int(11) NOT NULL,
+  `winner_id` int(11) NULL,
   `category_id` int(11) NOT NULL,
-  `date_creation` datetime DEFAULT NULL,
-  `name_lot` char(30) DEFAULT NULL,
+  `date_creation` datetime NOT NULL,
+  `name_lot` char(30) NOT NULL,
   `description` text,
   `url_picture` varchar(100) DEFAULT NULL,
-  `start_price` int(5) DEFAULT NULL,
-  `date_finish` datetime DEFAULT NULL,
-  `bid` int(5) DEFAULT NULL,
+  `start_price` int(5) NOT NULL,
+  `date_finish` datetime NOT NULL,
   FOREIGN KEY (category_id)  REFERENCES category(id),
   FOREIGN KEY (user_id)  REFERENCES users(id),
   FOREIGN KEY (winner_id)  REFERENCES users(id)
 );
 
 CREATE TABLE IF NOT EXISTS `bids` (
-  `id` int(11) NOT NULL,
+  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `lot_id` int(11) NOT NULL,
   `bid_date` date DEFAULT NULL,
@@ -46,31 +45,3 @@ CREATE TABLE IF NOT EXISTS `bids` (
   FOREIGN KEY (lot_id)  REFERENCES lots(id)
 );
 
-
-ALTER TABLE `bids`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `categoty`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `category` (`category`);
-
-ALTER TABLE `lots`
-  ADD PRIMARY KEY `id` (`id`),
-  ADD KEY `name_lot` (`name_lot`);
-
-
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
-
-ALTER TABLE `bids`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `categoty`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `lots`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
