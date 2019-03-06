a2bdd0aa61094064e0e71916f2304a3254fd550a
nevalashka <b.rukavishnikov@gmail.com>
2019-03-06 17:16:26 +0300

    обработка формы регистрации 7.1

--- sign-up.php
+++ sign-up.php
@@ -1,92 +1,120 @@
-<?php
+    <?php
 
-require_once("init.php");
-require_once("functions.php");
-require_once("data.php");
+    require_once("init.php");
+    require_once("functions.php");
+    require_once("data.php");
 
-$category = "";
-$required_fields = ['email', 'password', 'name', 'contact'];
+    $category = "";
+    $required_fields = ['email', 'password', 'name', 'contact'];
 
-if (!$link) {
-    $content = error_content();
-} else {
-    $sql_category = 'SELECT `id`, `category` FROM category';
-    $result_category = mysqli_query($link, $sql_category);
+    if (!$link) {
+        $content = error_content();
+    } else {
+        $sql_category = 'SELECT `id`, `category` FROM category';
+        $result_category = mysqli_query($link, $sql_category);
 
-    if($result_category) {
-        $category = fetch_all($result_category);
+        if($result_category) {
+           $category = fetch_all($result_category);
 
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-            $users = $_POST;
+                $users = $_POST;
 
-			if (!empty(($_FILES)) && ($_FILES['url_picture']['error']) == 0) {
-                $file_extension = new SplFileInfo($_FILES['url_picture']['name']);
-                $filename = uniqid().$file_extension;
-				$tmp_name = $_FILES['url_picture']['tmp_name'];
-				$path = 'img/'.$filename;
-				$finfo = finfo_open(FILEINFO_MIME_TYPE);
-				$file_type = finfo_file($finfo, $tmp_name);
-				if ($file_type !== "image/jpg" && $file_type !== "image/jpeg" && $file_type !== "image/png") {
-					$errors['url_picture'] = 'Изображение должно быть в формате .jpg/jpeg или .png';
-				} else {
-					move_uploaded_file($tmp_name, $path);
-					$lot['url_picture'] = $path;
-				}
-			} else {
-				$errors['url_picture'] = "Загрузите изображение лота";
-			}
+                if (!empty(($_FILES)) && ($_FILES['avatar']['error']) == 0) {
+                    $file_extension = new SplFileInfo($_FILES['avatar']['name']);
+                    $filename = uniqid().$file_extension;
+                    $tmp_name = $_FILES['avatat']['tmp_name'];
+                    $path = 'img/'.$filename;
+                    $finfo = finfo_open(FILEINFO_MIME_TYPE);
+                    $file_type = finfo_file($finfo, $tmp_name);
+                    if ($file_type !== "image/jpg" && $file_type !== "image/jpeg" && $file_type !== "image/png") {
+                        $errors['avatar'] = 'Изображение должно быть в формате .jpg/jpeg или .png';
+                    } else {
+                        move_uploaded_file($tmp_name, $path);
+                        $users['avatar'] = $path;
+                    }
+                } else {
+                    $errors['avatar'] = "Загрузите изображение лота";
+                }
 
-            if(empty($errors)) {
-                $sql_insert = 'INSERT INTO users (registration_date, email, password, name, avatar, contact)
-                VALUES (NOW(), ?, ?, ?, ?, ?)';
+                if (empty($users['name'])) {
+                    $errors['name'] = "Укажите имя пользователя";
+                }
 
-                $stmt = db_get_prepare_stmt($link, $sql_insert, [
-                    $users['email'],
-                    $users['password'],
-                    $users['name'],
-                    $users['avatar'],
-                    $users['contact'],
+                if (empty($users['contact'])) {
+                    $errors['contact'] = "Укажите контакты пользователя";
+                }
 
-                ]);
+                if (empty($users['email'])) {
+                    $errors['email'] = "Укажите email пользователя";
+                }
 
-                $res = mysqli_stmt_execute($stmt);
+                if (empty($users['password'])) {
+                    $errors['password'] = "Укажите пароль пользователя";
+                }
 
-                if ($res) {
-                    $lot_id = mysqli_insert_id($link);
-                    header("Location: login.php");
+                if(empty($errors)) {
+                    $email = mysqli_real_escape_string($link, $users);
+
+                    $sql_search_email = "SELECT id FROM users WHERE email = 'email'";
+
+                    $res_users = mysqli_query($link, $sql_search_email);
+
+                    if(mysqli_num_rows($res_users) > 0) {
+                        $errors[]= 'Пользователь с таким email уже зарегистрирован';
+                    }
+
+                    else {
+                        $password = password_hash($users['password'], PASSWORD_DEFAULT);
+                    }
+
+                    $sql_insert = 'INSERT INTO users (registration_date, email, password, name, avatar, contact)
+                    VALUES (NOW(), ?, ?, ?, ?, ?)';
+
+                    $stmt = db_get_prepare_stmt($link, $sql_insert, [
+                        $users['email'],
+                        $users['password'],
+                        $users['name'],
+                        $users['avatar'],
+                        $users['contact'],
+
+                    ]);
+
+                    $res = mysqli_stmt_execute($stmt);
+
+                    if ($res) {
+                        header("Location: login.php");
+                    }
+                    else {
+                        $content = error_content($link);
+                    }
                 }
                 else {
-                    $content = error_content($link);
+                    $content = include_template('sign-up.php', [
+                        'categories' => $category,
+                        'errors' => $errors,
+                    ]);
                 }
             }
+
             else {
                 $content = include_template('sign-up.php', [
-                    'categories' => $category,
-                    'errors' => $errors,
-                    'users' => $users
+                    'categories' => $category
                 ]);
             }
         }
-
         else {
-            $content = include_template('sign-up.php', [
-                'categories' => $category
-            ]);
+            $content = error_content($link);
         }
     }
-    else {
-        $content = error_content($link);
-    }
-}
 
-$layout = include_template("layout.php", [
-    "content" => $content,
-    "user_name" => $user_name,
-    "title" => $title,
-    "is_auth" => $is_auth,
-    "categories" => $category
-]);
-print($layout);
+    $layout = include_template("layout.php", [
+        "content" => $content,
+        "user_name" => $user_name,
+        "title" => $title,
+        "is_auth" => $is_auth,
+        "categories" => $category
+    ]);
+    print($layout);
 
-?>
+    ?>
