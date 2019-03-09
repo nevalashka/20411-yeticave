83c74cd0d83cffd178f67e0b08dc2deba4a81104
nevalashka <b.rukavishnikov@gmail.com>
2019-03-09 17:15:49 +0300

    Вход на сайт 7.2

--- login.php
+++ login.php
@@ -8,6 +8,7 @@ $title = "Вход - YetiCave";
 
 session_start();
 
+$errors = [];
 $category = "";
 $required_fields = ['email', 'password'];
 
@@ -22,60 +23,42 @@ if (!$link) {
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $login = $_POST;
-			}
 
-        if (empty(login['email']) && !filter_var($login['email'], FILTER_VALIDATE_EMAIL))) {
+            if (empty($login['email']) && !filter_var($login['email'], FILTER_VALIDATE_EMAIL)) {
                 $errors['email'] = "Введите email пользователя";
             }
 
-        if (empty(login['password'])) {
+            if (empty($login['password'])) {
                 $errors['password'] = "Введите пароль";
             }
 
+            $email = mysqli_real_escape_string($link, $login['email']);
+            $sql_mail = "SELECT * FROM users WHERE email = '$email'";
+            $res = mysqli_query($link, $sql_mail);
 
+            $user = $res ? mysqli_fetch_array($res, MYSQLI_ASSOC) : null;
 
-        if(empty($errors)) {
-            $sql_insert = 'INSERT INTO lots (date_creation, category_id, user_id, name_lot, description, url_picture, start_price, date_finish, bid_step )
-                VALUES (NOW(), ?, ?, ?, ?, ?, ?, ?, ?)';
-
-            $stmt = db_get_prepare_stmt($link, $sql_insert, [
-                     $lot['category'],
-                     $lot['user_id'],
-                     $lot['name_lot'],
-                     $lot['description'],
-                     $lot['url_picture'],
-                     $lot['start_price'],
-                     $lot['date_finish'],
-                     $lot['bid_step']
-                ]);
-
-                $res = mysqli_stmt_execute($stmt);
-
-                if ($res) {
-                    $lot_id = mysqli_insert_id($link);
-                    header("Location: lot.php?id=". $lot_id);
+            if(empty($errors) && !empty($user)) {
+                if (password_verify($login['password'], $login['password'])) {
+                    $_SESSION['user'] = $user;
+                    header("Location: index.php");
+                    exit();
                 }
                 else {
-                    $content = error_content($link);
+                    $errors['password'] = 'Неверный пароль';
                 }
             }
-            else {
-                $content = include_template('login.php', [
-                    'categories' => $category,
-                    'errors' => $errors,
-                    'lot' => $lot
-                ]);
-            }
+            $content = include_template('login.php', [
+               'categories' => $category,
+               'login' => $login,
+               'errors' => $errors
+            ]);
         }
-
         else {
             $content = include_template('login.php', [
                 'categories' => $category
             ]);
         }
-    }
-    else {
-        $content = error_content($link);
     }
 }
 
--- templates/login.php
+++ templates/login.php
@@ -8,19 +8,17 @@
         <?php endforeach; ?>
     </ul>
 </nav>
-    <form class="form container <?= (isset($errors)) ? 'form--invalid' : '';?>" action="login.php" method="post" enctype="multipart/form-data">
-        <!-- form--invalid -->
-      <h2>Вход</h2>
-      <div class="form__item <?= (isset($errors['email'])) ? 'form__item--invalid' : '';?>">
+<form class="form container <?= (isset($errors)) ? 'form--invalid' : '';?>" action="login.php" method="post" enctype="multipart/form-data">
+    <h2>Вход</h2>
+    <div class="form__item <?= (isset($errors['email'])) ? 'form__item--invalid' : '';?>">
         <label for="email">E-mail*</label>
-        <input id="email" type="text" name="email" placeholder="Введите e-mail"
-               value="<?= (isset($users['email'])) ? $users['email'] : ''; ?>">
+        <input id="email" type="text" name="email" placeholder="Введите e-mail" value="<?= (isset($login['email'])) ? $login['email'] : ''; ?>">
         <span class="form__error">Введите e-mail</span>
-      </div>
-      <div class="form__item form__item--last">
+    </div>
+    <div class="form__item form__item--last <?= (isset($errors['password'])) ? 'form__item--invalid' : '';?>">
         <label for="password">Пароль*</label>
-        <input id="password" type="password" name="password" placeholder="Введите пароль" required>
+        <input id="password" type="password" name="password" placeholder="Введите пароль">
         <span class="form__error">Введите пароль</span>
-      </div>
-      <button type="submit" class="button">Войти</button>
-    </form>
+    </div>
+    <button type="submit" class="button">Войти</button>
+</form>
