bfc1f7cc87cc7f5e536f8617fda4ac4f34486efe
nevalashka <b.rukavishnikov@gmail.com>
2019-03-11 01:58:45 +0300

    ввод и отображение ставок 8.1

--- lot.php
+++ lot.php
@@ -16,28 +16,50 @@ if (!$link) {
 
         if(isset($_GET['id'])) {
             $lot_id = $_GET['id'];
-        $lots_sql = 'SELECT * FROM lots l JOIN category c ON l.category_id = c.id WHERE l.id="'.$lot_id.'"';
-        $lots_result = mysqli_query($link, $lots_sql);
+            $lots_sql = 'SELECT * FROM lots l JOIN category c ON l.category_id = c.id WHERE l.id="'.$lot_id.'"';
+            $lots_result = mysqli_query($link, $lots_sql);
 
-        if (!empty($lots_result)) {
+            if (!empty($lots_result)) {
             $category = fetch_all($result);
             $lots = fetch_all($lots_result);
 
-            $content = include_template("lot.php",
-            [
-                "lot" => $lots,
-                "categories" => $category
-            ]);
-        }
-        else {
-            $content = error_content($link);
-        }
+          //  $start_price_bid_step_sql = 'SELECT start_price, bid_step FROM lots WHERE lot_id = $_POST['id']';
+        //    $bids_on_lot = 'SELECT bid_amount FROM bids WHERE lot_id = $_POST['id']';
 
-    }
+                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                    $bid = $_POST;
+                    if (empty($bid['bid']) && !validate_number($bid['bid'])) {
+                        $errors['bid'] = 'Введите целое число больше 0';
+                    }
+                    if ($bid['bid'] <= $lot['bid_step']) {
+                        $errors['bid'] = 'введите ставку больше, чем прошлая';
+                    }
+                    else {
+                        $errors['bid'] = 'введите ставку больше, чем текущая цена лота плюс шаг ставки';
+                    }
+                    if(empty($bids_on_lot)) {
+                  //      $sql_bid_insert = INSERT INTO bids (bid_amount, user_id, lot_id)
+                    //        VALUES ($bid['start_price'].$bid['bid_step'], 'user_id', $lot_id);
+                    }
+                    else {
+                      //  $sql_bid_insert = INSERT INTO bids (bid_amount, user_id, lot_id)
+                        //    VALUES ($bid['bid_step'] /*В новую ставку записываем сумму последней ставки с введенным шагом, вообще не понял как взять последнию ставку */ );
+                    }
+                }
+            $content = include_template("lot.php",[
+                "lot" => $lots,
+                "categories" => $category]);
+            }
+            else {
+                $content = error_content($link);
+            }
+        }
     else {
         $content = error_content($link);
     }
+
 }
+
 $layout = include_template("layout.php", [
     "content" => $content,
     "user_name" => $user_name,
--- templates/lot.php
+++ templates/lot.php
@@ -38,15 +38,70 @@
                         Мин. ставка <span>12 000 р</span>
                     </div>
                 </div>
-                <form class="lot-item__form" action="https://echo.htmlacademy.ru" method="post">
-                    <p class="lot-item__form-item form__item form__item--invalid">
+                <form class="lot-item__form" action="lot.php" method="post">
+                    <p class="lot-item__form-item form__item <?= (isset($errors['bid'])) ? 'form__item--invalid' : '';?>">
                         <label for="cost">Ваша ставка</label>
-                        <input id="cost" type="text" name="cost" placeholder="12 000">
+                        <input id="cost" type="text" name="bid" placeholder="12 000">
                         <span class="form__error">Введите наименование лота</span>
                     </p>
                     <button type="submit" class="button">Сделать ставку</button>
                 </form>
             </div>
+            <div class="history">
+            <h3>История ставок (<span>10</span>)</h3>
+            <table class="history__list">
+              <tr class="history__item">
+                <td class="history__name">Иван</td>
+                <td class="history__price">10 999 р</td>
+                <td class="history__time">5 минут назад</td>
+              </tr>
+              <tr class="history__item">
+                <td class="history__name">Константин</td>
+                <td class="history__price">10 999 р</td>
+                <td class="history__time">20 минут назад</td>
+              </tr>
+              <tr class="history__item">
+                <td class="history__name">Евгений</td>
+                <td class="history__price">10 999 р</td>
+                <td class="history__time">Час назад</td>
+              </tr>
+              <tr class="history__item">
+                <td class="history__name">Игорь</td>
+                <td class="history__price">10 999 р</td>
+                <td class="history__time">19.03.17 в 08:21</td>
+              </tr>
+              <tr class="history__item">
+                <td class="history__name">Енакентий</td>
+                <td class="history__price">10 999 р</td>
+                <td class="history__time">19.03.17 в 13:20</td>
+              </tr>
+              <tr class="history__item">
+                <td class="history__name">Семён</td>
+                <td class="history__price">10 999 р</td>
+                <td class="history__time">19.03.17 в 12:20</td>
+              </tr>
+              <tr class="history__item">
+                <td class="history__name">Илья</td>
+                <td class="history__price">10 999 р</td>
+                <td class="history__time">19.03.17 в 10:20</td>
+              </tr>
+              <tr class="history__item">
+                <td class="history__name">Енакентий</td>
+                <td class="history__price">10 999 р</td>
+                <td class="history__time">19.03.17 в 13:20</td>
+              </tr>
+              <tr class="history__item">
+                <td class="history__name">Семён</td>
+                <td class="history__price">10 999 р</td>
+                <td class="history__time">19.03.17 в 12:20</td>
+              </tr>
+              <tr class="history__item">
+                <td class="history__name">Илья</td>
+                <td class="history__price">10 999 р</td>
+                <td class="history__time">19.03.17 в 10:20</td>
+              </tr>
+            </table>
+          </div>
             <?php endif; ?>
         </div>
     </div>
